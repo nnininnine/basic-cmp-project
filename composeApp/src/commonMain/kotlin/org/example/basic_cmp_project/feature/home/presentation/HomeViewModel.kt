959023b0f54c7a7d3d6d4d9b58132c6ceb12f63e
Nuttapon Buaban <nuttapon.dev@gmail.com>
2025-04-08 16:45:25 +0700

    feat: add save state into HomeViewModel

--- composeApp/src/commonMain/kotlin/org/example/basic_cmp_project/feature/home/presentation/HomeViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/example/basic_cmp_project/feature/home/presentation/HomeViewModel.kt
@@ -1,18 +1,23 @@
 package org.example.basic_cmp_project.feature.home.presentation
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
-class HomeViewModel: ViewModel() {
-    private val _uiState = MutableStateFlow(UiState())
+class HomeViewModel(
+    private val state: SavedStateHandle
+) : ViewModel() {
+    private val _uiState = MutableStateFlow(UiState(showContent = state.get<Boolean>("showContent") ?: false))
     val uiState = _uiState.asStateFlow()
 
     fun toggleShowContent() {
-        _uiState.value = UiState(showContent = !_uiState.value.showContent)
+        state["showContent"] = !_uiState.value.showContent
+        _uiState.update { it.copy(showContent = !it.showContent) }
     }
 
     data class UiState(
-        val showContent: Boolean = false
+        val showContent: Boolean = false,
     )
 }
